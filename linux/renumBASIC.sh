09b2e4d6f311eecd103e059a83bf5fc5243663b5
Ron Klein <ron@kdomain.org>
2019-07-11 08:04:13 -0500

    Add new script to renumber BASIC programs

---
+++ linux/renumBASIC.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# script to renumber a BASIC program
+
+# more information about this utility can be found here:
+# https://github.com/tilleul/basic_renumber
+
+nodejs $HOME/source/basic_renumber/renumber.js $1 $2 10
+
+
