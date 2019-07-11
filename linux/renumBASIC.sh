879e544a3c56aef1fe934c926f99225b9a66e13b
Ron Klein <ron@kdomain.org>
2019-07-11 08:12:13 -0500

    Minor changes to BASIC renumber script

--- linux/renumBASIC.sh
+++ linux/renumBASIC.sh
@@ -5,6 +5,9 @@
 # more information about this utility can be found here:
 # https://github.com/tilleul/basic_renumber
 
-nodejs $HOME/source/basic_renumber/renumber.js $1 $2 10
+# 3 parameters are required
+# original filename, new filename, renumber increcment value
+
+nodejs $HOME/source/basic_renumber/renumber.js $1 $2 $3
 
 
